91a01746c072087a2a2a6457d3ddf34e9abd5714
Sir Robert Burbridge <sirrobert@gmail.com>
2020-07-09 20:30:14 -0400

    Updates

---
+++ precache-manifest.dec642788b7361559fb7042d87267408.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f7088ae93ec1cf7c03eaaea410dff7a",
+    "url": "/commando/index.html"
+  },
+  {
+    "revision": "12cb9955d75e556c57bf",
+    "url": "/commando/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "7facbdbbde46143139bc",
+    "url": "/commando/static/js/2.f76765ad.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/commando/static/js/2.f76765ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "12cb9955d75e556c57bf",
+    "url": "/commando/static/js/main.6bb69e86.chunk.js"
+  },
+  {
+    "revision": "a0164f8783d507d8853c",
+    "url": "/commando/static/js/runtime-main.e53e547c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/commando/static/media/logo.5d5d9eef.svg"
+  }
+]);
